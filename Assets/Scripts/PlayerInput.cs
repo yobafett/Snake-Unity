c980e75b6bb697be7a8b6c08a19e160138f652d8
Yoba_Fett <alexka.680@gmail.com>
2021-11-02 19:51:24 +0900

    Player input refactoring

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,7 +5,7 @@ using UnityEngine.Serialization;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
-    [SerializeField] private SnakeMover[] movers;
+    [SerializeField] private SnakeMover mover;
     
     private void Update()
     {
@@ -14,29 +14,11 @@ public class PlayerInput : MonoBehaviour
 
     private void Move()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(mainCamera.transform.position, ray.direction * 100f, Color.red);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
         {
-            var targetX = hit.point.x;
-            targetX = Mathf.Clamp(targetX,-1f, 1f);
-
-            foreach (var mover in movers)
-            {
-                mover.SetMoveTarget(targetX);
-            }
+            var targetX = Mathf.Clamp(hit.point.x, -1f, 1f);
+            mover.SetMoveTarget(targetX);
         }
-    }
-    
-    private Vector3 DrawRayToMousePosition(Vector3 mousePosition)
-    {
-        mousePosition.z = mainCamera.nearClipPlane + 0.01f;
-        var position = mainCamera.ScreenPointToRay(mousePosition);
-        
-        Debug.DrawLine(position.origin, position.direction - (Vector3.back * 5f), Color.red);
-
-        return position.direction;
     }
 }
