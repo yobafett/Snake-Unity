c10ee173cb490ac9f130b9ec75fe8eb14dfb03d8
Yoba_Fett <alexka.680@gmail.com>
2021-11-02 20:27:09 +0900

    Add snake tail moving

--- Assets/Scripts/SnakeMover.cs
+++ Assets/Scripts/SnakeMover.cs
@@ -6,26 +6,37 @@ using UnityEngine.Serialization;
 
 public class SnakeMover : MonoBehaviour
 {
-    [SerializeField] private float lerpTime;
+    [SerializeField][Range(2f, 10f)] private float maxSpeed;
+    [SerializeField][Range(0.1f, 2f)] private float minSpeed;
+    [SerializeField] private Transform[] tail;
+    private Transform _transform;
     private Vector3 _moveTarget;
     private bool _isMoving;
     private float _progress;
-    private Transform _transform;
     private float _defaultZ;
     private float _defaultY;
-    
+    private float _decreaseModifier;
+
     private void Start()
     {
         _transform = transform;
         var lPos = _transform.localPosition;
         _defaultZ = lPos.z;
         _defaultY = lPos.y;
+        _decreaseModifier = (maxSpeed - minSpeed) / tail.Length;
     }
-
     
     private void Update()
     {
-        if (_isMoving) Moving();
+        if (_isMoving)
+        {
+            Moving(_transform, maxSpeed, _moveTarget);
+            for (var i = 0; i < tail.Length; i++)
+            {
+                Moving(tail[i], maxSpeed - (_decreaseModifier * (i + 1)),
+                    new Vector3(_moveTarget.x, _defaultY, tail[i].position.z));
+            }
+        }
     }
 
     public void SetMoveTarget(float targetX)
@@ -35,11 +46,11 @@ public class SnakeMover : MonoBehaviour
         _isMoving = true;
     }
     
-    private void Moving()
+    private void Moving(Transform objectTransform, float speed, Vector3 moveTarget)
     {
-        transform.position = Vector3.Lerp(transform.position, _moveTarget, lerpTime * Time.deltaTime);
-        _progress = Mathf.Lerp(_progress, 1f, lerpTime * Time.deltaTime);
-        if (_progress > 0.9f)
+        objectTransform.position = Vector3.Lerp(objectTransform.position, moveTarget, speed * Time.deltaTime);
+        _progress = Mathf.Lerp(_progress, 1f, speed * Time.deltaTime);
+        if (_progress > 0.99f)
         {
             _isMoving = false;
         }
